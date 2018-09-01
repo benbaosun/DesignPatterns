6c154df3babb3fda9801c430fe76734c93be4ab0
lkmc2 <lkmc2@163.com>
2018-09-01 22:21:13 +0800

    采用急切式方法创建单例

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -6,17 +6,13 @@ package SingletonPattern;
  * @description 单例
  */
 public class Singleton {
-    private static Singleton instance;
+    private static Singleton instance = new Singleton();
     private int count; // 访问次数
 
     private Singleton() {
     }
 
     public static synchronized Singleton getInstance() {
-        // 如果访问时问创建实例对象，则创建
-        if (instance == null) {
-            instance = new Singleton();
-        }
         return instance;
     }
 
