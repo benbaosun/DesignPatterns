7a74ecd43dcd6003fb105c023d7d60fef9538878
lkmc2 <lkmc2@163.com>
2018-09-02 16:21:21 +0800

    添加适配器类

---
+++ src/ApapterPattern/AdapterTest.java
@@ -0,0 +1,54 @@
+package ApapterPattern;
+
+/**
+ * @author lkmc2
+ * @date 2018/9/2
+ * @description 适配器测试类
+ */
+public class AdapterTest {
+
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck(); // 绿头鸭
+        WildTurkey turkey = new WildTurkey(); // 火鸡
+
+        // 将火鸡冒充成鸭子
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("街头顽禽说：");
+        turkey.gobble(); // 咯咯叫
+        turkey.fly(); // 飞行
+
+        System.out.println("\n鸭子说：");
+        testDuck(duck);
+
+        System.out.println("\n冒充成鸭子的街头顽禽说：");
+        testDuck(turkeyAdapter);
+
+        /*
+            运行结果：
+            街头顽禽说：
+            咯咯叫
+            我会飞
+
+            鸭子说：
+            呱呱叫
+            我会飞
+
+            冒充成鸭子的街头顽禽说：
+            咯咯叫
+            我会飞
+            我会飞
+            我会飞
+            我会飞
+            我会飞
+         */
+
+    }
+
+    // 测试鸭子类
+    private static void testDuck(Duck duck) {
+        duck.quack(); // 叫唤
+        duck.fly(); // 飞行
+    }
+
+}
---
+++ src/ApapterPattern/TurkeyAdapter.java
@@ -0,0 +1,28 @@
+package ApapterPattern;
+
+/**
+ * @author lkmc2
+ * @date 2018/9/2
+ * @description 街头顽禽适配器（实现鸭子接口）
+ * 缺少鸭子，需要拿街头顽禽冒充
+ */
+public class TurkeyAdapter implements Duck {
+    private Turkey turkey; // 街头顽禽
+
+    public TurkeyAdapter(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+    @Override
+    public void quack() {
+        turkey.gobble(); // 咯咯叫
+    }
+
+    @Override
+    public void fly() {
+        // 街头顽禽需要飞行五次才能达到和鸭子的效果
+        for (int i = 0; i < 5; i++) {
+            turkey.fly();
+        }
+    }
+}
