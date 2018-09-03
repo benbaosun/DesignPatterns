26c15ed4800cd988290ebf8180aa9ffea28f3fb2
lkmc2 <lkmc2@163.com>
2018-09-03 15:51:53 +0800

    使用列表简化服务员类中的代码

--- src/IteratorPattern/Waitress.java
+++ src/IteratorPattern/Waitress.java
@@ -4,6 +4,7 @@ import IteratorPattern.menu.Menu;
 import IteratorPattern.menu.MenuItem;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author lkmc2
@@ -11,34 +12,18 @@ import java.util.Iterator;
  * @description 服务员
  */
 public class Waitress {
-    private Menu pancakeHouseMenu; // 煎饼屋的菜单
-    private Menu dinerMenu; // 餐厅菜单
-    private Menu cafeMenu; // 咖啡厅菜单
+    private List<Menu> menus; // 各餐厅的菜单列表
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(List<Menu> menus) {
+        this.menus = menus;
     }
 
     // 打印全部菜单
     public void printMenu() {
-        // 创建煎饼屋菜单的迭代器
-        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
-        // 创建餐厅菜单的迭代器
-        Iterator dinerIterator = dinerMenu.createIterator();
-        // 创建咖啡厅的迭代器
-        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
-
-        System.out.println("----- 菜单 -----");
-        System.out.println("早餐：");
-        printMenu(pancakeIterator);
-        System.out.println("----------------");
-        System.out.println("午餐：");
-        printMenu(dinerIterator);
-        System.out.println("----------------");
-        System.out.println("晚餐：");
-        printMenu(cafeIterator);
+        for (Menu menu : menus) {
+            // 创建菜单的迭代器并打印
+            printMenu(menu.createIterator());
+        }
     }
 
     // 打印迭代器里面的菜单
