d6ac084632e062c490f6a09a7c6860062c5b1db4
lkmc2 <lkmc2@163.com>
2018-09-03 23:35:38 +0800

    进一步测试监视器的效果

--- src/ProxyPattern/GumballMonitorTest.java
+++ src/ProxyPattern/GumballMonitorTest.java
@@ -10,29 +10,52 @@ import java.util.Scanner;
 public class GumballMonitorTest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("请输入糖果数目：");
-        int count = scanner.nextInt();
-
         // 糖果机
-        GumballMachine machine = new GumballMachine(count, "北京王府井");
+        GumballMachine machine = new GumballMachine(5, "北京王府井");
         // 糖果机监视器
         GumballMonitor monitor = new GumballMonitor(machine);
+        monitor.report(); // 监视器打印糖果机报告
 
-        // 打印糖果机报告
-        monitor.report();
+        decreaseOneCandy(machine); // 减去一颗糖果
+        monitor.report();  // 监视器打印糖果机报告
+
+        decreaseOneCandy(machine); // 减去一颗糖果
+        monitor.report();  // 监视器打印糖果机报告
 
         /*
             运行状态：
-            请输入糖果数目：
-            9
             ------ 糖果机报告------
             糖果机位置：北京王府井
-            剩余糖果数：9
+            剩余糖果数：5
+            当前状态：糖果机状态[未投币]
+            ******************
+            已投币
+            已移动曲柄
+            已弹出糖果
+            ******************
+            ------ 糖果机报告------
+            糖果机位置：北京王府井
+            剩余糖果数：4
+            当前状态：糖果机状态[未投币]
+            ******************
+            已投币
+            已移动曲柄
+            已弹出糖果
+            ******************
+            ------ 糖果机报告------
+            糖果机位置：北京王府井
+            剩余糖果数：3
             当前状态：糖果机状态[未投币]
          */
 
+    }
+
+    // 减去一颗糖果
+    private static void decreaseOneCandy(GumballMachine machine) {
+        System.out.println("******************");
+        machine.insertQuarter(); // 投币
+        machine.turnCrank(); // 移动曲柄（移动曲柄才会发放糖果）
+        System.out.println("******************");
     }
 
 }
