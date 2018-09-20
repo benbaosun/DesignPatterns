b9a8fb7a688a70100dacc3c62e514d55dee67f83
lkmc2 <lkmc2@163.com>
2018-09-20 22:55:55 +0800

    添加不使用Builder模式，只使用Getter和Setter创建对象的例子

--- src/BuilderPattern/Person.java
+++ src/BuilderPattern/Person.java
@@ -15,48 +15,59 @@ public class Person {
     private String address;
     private String desc;
 
-    public Person(int id, String name) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public Person(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public Person(int id, String name, int age, String sex) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
-    public Person(int id, String name, int age, String sex, String phone) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public Person(int id, String name, int age, String sex, String phone, String address) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        this.phone = phone;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public Person(int id, String name, int age, String sex, String phone, String address, String desc) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        this.phone = phone;
-        this.address = address;
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
         this.desc = desc;
     }
 
--- src/BuilderPattern/TestMain.java
+++ src/BuilderPattern/TestMain.java
@@ -8,28 +8,20 @@ package BuilderPattern;
 public class TestMain {
 
     public static void main(String[] args) {
-        Person person1 = new Person(1, "Jack");
-        Person person2 = new Person(2, "Andy", 22);
-        Person person3 = new Person(3, "Wang", 18, "男");
-        Person person4 = new Person(4, "Hack", 18, "男", "18755668899");
-        Person person5 = new Person(5, "Kim", 18, "女", "18679002635", "玉林");
-        Person person6 = new Person(6, "Cindy", 18, "女", "13726594488", "北海", "一个善良的人");
+        Person person = new Person();
+        person.setId(1);
+        person.setName("Jack");
+        person.setAge(18);
+        person.setSex("男");
+        person.setPhone("18745694458");
+        person.setAddress("北海");
+        person.setDesc("一个善良的人");
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-        System.out.println(person4);
-        System.out.println(person5);
-        System.out.println(person6);
+        System.out.println(person);
 
         /*
             运行结果：
             Person{id=1, name='Jack', age=0, sex='null', phone='null', address='null', desc='null'}
-            Person{id=2, name='Andy', age=22, sex='null', phone='null', address='null', desc='null'}
-            Person{id=3, name='Wang', age=18, sex='男', phone='null', address='null', desc='null'}
-            Person{id=4, name='Hack', age=18, sex='男', phone='18755668899', address='null', desc='null'}
-            Person{id=5, name='Kim', age=18, sex='女', phone='18679002635', address='玉林', desc='null'}
-            Person{id=6, name='Cindy', age=18, sex='女', phone='13726594488', address='北海', desc='一个善良的人'}
          */
     }
 
